96a01a501c01a91523008e4babf2bd9aa883869d
Regina0813 <97012789+Regina0813@users.noreply.github.com>
2022-09-11 22:13:31 -0500

    Refactor from command line to a Web App Testing

--- src/main/java/com/revature/user/AppContext.java
+++ src/main/java/com/revature/user/AppContext.java
@@ -9,30 +9,30 @@ import java.io.InputStreamReader;
 public class AppContext {
 
     private static boolean appRunning;
-   // private BufferedReader consoleReader;
+    private BufferedReader consoleReader;
 
     public AppContext() {
         appRunning = true;
-      //  consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        consoleReader = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public void startApp() {
         while (appRunning) {
-            try {
-              //  System.out.println("The app is started, but will close immediately.");
-                UserDAO userDAO = new UserDAO();
-                ErsUsers loggedInUsed = userDAO.findUserByUsernameAndPassword("aanderson", "p4$$W0RD")
-                        .orElseThrow(() -> new RuntimeException("No user found with the provided credentials"));
 
-                System.out.println("Successfully logged in user: " + loggedInUsed);
+            try {
+                System.out.println("The App is Running, but will stop");
                 appRunning = false;
             } catch (Exception e) {
                 e.printStackTrace();
-                appRunning = false;
             }
         }
     }
+
+
     public static void shutdown() {
         appRunning = false;
     }
+
+
+
 }
